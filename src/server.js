cf72e89c046ab6abc86b13cfe2b998f2e51f75ab
Jonathan Scheffel <jonathan.scheffel@tutanota.com>
2022-05-09 04:39:38 -0400

    init

---
+++ src/server.js
@@ -0,0 +1,23 @@
+import Hapi from "@hapi/hapi";
+import path from "path";
+
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+async function init() {
+  const server = Hapi.server({
+    port: 3000,
+    host: "localhost",
+  });
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
+}
+
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
